05324b515168060026bf35df6fac5cc2de6a86e3
LucasVY <lucasvy@LucasVYs-MacBook-Pro.local>
2021-04-09 15:59:56 -0400

    reproductor final

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState, useRef} from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css'
 import PlayList from './Components/PlayList'
 
 function App() {
@@ -8,19 +9,18 @@ function App() {
   const [state] = useState( PlayList )
 
   const [songsActive, setsongsActive] = useState(null); 
-  let nombreRef = useRef(null); 
+  let nameRef = useRef(null); 
   
-
   const setSingleSong = (linkName, i) => {
-    nombreRef.src = `https://assets.breatheco.de/apis/sound/${linkName}`;
+    nameRef.src = `https://assets.breatheco.de/apis/sound/${linkName}`;
     setsongsActive(i) 
   }
   const playsong = () => {
-    if (nombreRef !== null)
-    nombreRef.play();
+    if (nameRef !== null)
+    nameRef.play();
   }
   const pause = () => {
-    nombreRef.pause();
+    nameRef.pause();
   }
   const nextSong = () => {
     let position= songsActive !== null ? songsActive === state.length - 1 ? 0 : songsActive + 1 : 0;
@@ -40,13 +40,13 @@ function App() {
         <div className="col-md-6 mx-auto  text-align">
           <div className="list-group text-align">
             {
-              state.length > 0 && state.map((value, j) => {
-                return <button key={value.id} id="list-btn" className={"btn btn-lg text-left bg-dark shadow-1-strong" + (songsActive === j)} onClick={() => setSingleSong(value.url, j)} >{value.name} </button>  
+              state.length > 0 && state.map((value, indexList) => {
+                return <button key={value.id} id="list-btn" className={"btn btn-lg text-left bg-dark shadow-1-strong" + (songsActive === indexList)} onClick={() => setSingleSong(value.url, indexList)} >{value.name} </button>  
               })
             }
           </div>
                 <div className="col-mt-4 bg-dark mx-auto text-center list-group-item list-group-item-action">
-              <audio className="audioControl" ref={r => nombreRef = r} autoPlay controls/>
+              <audio className="audioControl" ref={r => nameRef = r} autoPlay controls/>
               <button className="btn-sm bg-dark" onClick={rewindSong}><i className="fas fa-step-backward"></i></button>
               <button className="btn-sm bg-dark" onClick={pause}><i className="fas fa-pause"></i></button>
               <button className="btn-sm bg-dark" onClick={playsong}><i className="fas fa-play"></i></button>
